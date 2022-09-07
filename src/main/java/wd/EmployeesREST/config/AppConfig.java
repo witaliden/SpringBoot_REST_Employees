483f8e8da04040d32fed63d427b81885f4d16e71
WD <52528405+witaliden@users.noreply.github.com>
2022-09-07 21:34:57 +0200

    1. Logging refactoring. 2. Actuator enabled. 3. First steps to swagger.

--- src/main/java/wd/EmployeesREST/config/AppConfig.java
+++ src/main/java/wd/EmployeesREST/config/AppConfig.java
@@ -4,6 +4,12 @@ import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import wd.EmployeesREST.dao.EmployeeRepository;
 
 @Configuration
@@ -13,10 +19,39 @@ class AppConfig {
 
     @Bean
     @ConditionalOnProperty(name = "spring.datasource.url",
-    havingValue = "jdbc:postgresql://localhost:5432/mastery")
+            havingValue = "jdbc:postgresql://localhost:5432/mastery")
     @ConditionalOnBean(name = "EmployeeRepository")
     @ConditionalOnMissingBean
-    public void someBean(){
+    public void someBean() {
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
     }
 
 
