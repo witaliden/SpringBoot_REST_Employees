d6c84c0be55ea73e95f222522d418eb63f9180c9
witaliden <52528405+witaliden@users.noreply.github.com>
2023-05-10 16:58:53 +0200

    Changes backup

--- src/main/kotlin/com/employees/jms/EmployeeJmsConsumer.kt
+++ src/main/kotlin/com/employees/jms/EmployeeJmsConsumer.kt
@@ -4,19 +4,16 @@ import com.employees.config.JmsConfig
 import com.employees.dao.EmployeeRepository
 import com.employees.dto.Employee
 import mu.KotlinLogging
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.jms.annotation.JmsListener
+import org.springframework.jms.core.JmsTemplate
 import org.springframework.stereotype.Component
 
 private val log = KotlinLogging.logger {}
 
 @Component
-class EmployeeJmsConsumer(employeeRepository: EmployeeRepository) {
-    private val employeeRepository: EmployeeRepository
-
-    init {
-        this.employeeRepository = employeeRepository
-    }
-
+@EntityScan
+class EmployeeJmsConsumer(private val employeeRepository: EmployeeRepository, jmsTemplate: JmsTemplate) {
     @JmsListener(destination = JmsConfig.EMPLOYEES_QUEUE)
     fun consumeMessage(employee: Employee?) {
         log.info("IN: ConsumeMessage received employee {}", employee)
